97e29af80fd84b7a06594850a59f27d38f24ae67
koka <nikolay.zapolskikh@gmail.com>
2022-11-17 07:17:10 +0300

    6 sprint. 2 version

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,37 +1,28 @@
 #include "remove_duplicates.h"
 #include "log_duration.h"
-#include <tuple>
-#include <algorithm>
-#include <iterator>
 
 using namespace std;
 
 void RemoveDuplicates(SearchServer& search_server) {
-//	LOG_DURATION("RemoveDuplicates"s);
-	std::vector<int> duplicate_id;
-	duplicate_id.reserve(search_server.GetDocumentCount());
-	std::map<string, double> words_freq;
-	std::map<int, std::string> id_words;
+	//	LOG_DURATION("RemoveDuplicates"s);
+	std::set<int> duplicate_id;
+	std::map<std::set<string>, int> words_id;
 	
 	for (const int id : search_server) {
-		std::string doc;
-		for (const auto& [word, freq] : search_server.GetWordFrequencies(id)) {
-			doc += (word + " "s); //т.к. в словаре word упорядочены, то просто составляем строку из всех слов для документа для последующего сравнения
+		std::set<string> words;
+		for (const auto& [word, freqs] : search_server.GetWordFrequencies(id)) {
+			words.insert(word);
 		}
-		const auto it = find_if(id_words.begin(), id_words.end(), [&doc](const std::pair<int, std::string>& iw1) {
-			return iw1.second == doc;
-		});
-		if (it->first < id && it != id_words.end()) { //если текущий id больше найденного id совпадения, не добавляем в id_words, а вносим в вектор для дубликатов id
-			duplicate_id.push_back(id);
-			continue;
+		// если такое множество уже есть, заносим id в дубликаты
+		if (words_id.count(words) != 0) {
+			duplicate_id.insert(id);
 		} else {
-			id_words[id] = doc;
+			words_id[words] = id;
 		}
 	}
-	
+	//удаляем дубликаты по id
 	for (const int id : duplicate_id) {
 		cout << "Found duplicate document id "s << id << endl;
 		search_server.RemoveDocument(id);
 	}
 }
-   
