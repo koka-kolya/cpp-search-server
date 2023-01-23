7d99fcbb56fe5de766f2031c4821d126aac81e20
koka-kolya <112828943+koka-kolya@users.noreply.github.com>
2023-01-23 19:14:30 +0300

    Add files via upload
    9 sprint. first_attempt.

---
+++ process_queries.cpp
@@ -0,0 +1,20 @@
+#include "process_queries.h"
+
+std::vector<std::vector<Document>> ProcessQueries(
+        const SearchServer& search_server, const std::vector<std::string>& queries) {
+
+    std::vector<std::vector<Document>> output(queries.size());
+    std::transform(std::execution::par, queries.begin(), queries.end(), output.begin(), [&search_server] (const std::string& query) {return search_server.FindTopDocuments(query);});
+    return output;
+}
+
+std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries) {
+
+    std::vector<Document> output;
+    for (const auto& documents : ProcessQueries(search_server, queries)) {
+        output.insert(output.end(), documents.begin(), documents.end());
+    }
+    return output;
+}
+
+
---
+++ string_processing.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <set>
+#include <string>
+#include <vector>
+
+std::vector<std::string_view> SplitIntoWords(const std::string_view text);
+
+template <typename StringContainer>
+std::set<std::string, std::less<>> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string, std::less<>> non_empty_strings;
+    for (std::string_view str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.emplace(str);
+        }
+    }
+    return non_empty_strings;
+}
